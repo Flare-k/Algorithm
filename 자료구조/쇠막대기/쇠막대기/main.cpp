46e1965d9da402b5b0d55f4409a619f564957e63
강연욱 <rokkyw@khu.ac.kr>
2020-04-13 17:17:52 +0900

    Data_Structure

---
+++ 자료구조/쇠막대기/쇠막대기/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+using namespace std;
+
+int solution(string arrangement) {
+    stack<char> s;
+    int cnt = 0;
+  
+    for(int j=0; j<arrangement.length(); j++){
+        if(arrangement[j] == '(' || s.empty()){
+            s.push(arrangement[j]);    //push하여 꾹꾹 눌러담음
+        }
+        else if(s.top() == '('){  //이부분 해석중요
+            s.pop();
+            if(arrangement[j-1]=='(') cnt+=s.size();
+            else
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main(){
+    string icon;
+    cin>>icon;
+    
+    cout<<solution(icon)<<endl;
+    return 0;
+}
+
---
+++ 자료구조/프린터_큐_Prog/프린터_큐_Prog/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    priority_queue<int> pq;
+    queue<pair<int, int>> q;
+    for(int i=0; i<priorities.size(); i++){
+        q.push(make_pair(i, priorities[i]));        //인덱스와 값들을 집어넣기
+        pq.push(priorities[i]);
+    }
+    
+    while(!q.empty()){
+        int idx = q.front().first;
+        int p = q.front().second;
+        q.pop();
+        if(p == pq.top()){
+            answer += 1;
+            pq.pop();
+            
+            if(idx == location)
+                break;
+        }
+        else{
+            q.push(make_pair(idx, p));
+        }
+    }
+    return answer;
+}
+int main(){
+    vector<int> p{1, 1, 9, 1, 1, 1};
+    int loca;
+    cin>>loca;
+    cout<<solution(p, loca)<<endl;
+    
+    return 0;
+}
