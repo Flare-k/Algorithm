8375c480bdd30d7069eb09fd19762731bc98242f
강연욱 <rokkyw@khu.ac.kr>
2020-04-15 17:40:56 +0900

    섬의개수__DFSBFS_4963

--- 그래프/섬의개수_그래프_4963/섬의개수_그래프_4963/main.cpp
+++ 그래프/섬의개수_그래프_4963/섬의개수_그래프_4963/main.cpp
@@ -8,20 +8,19 @@ using namespace std;
 const int MAX = 1000+1;
 int arr[MAX][MAX];
 bool visit[MAX][MAX];
-int cnt,a, b;
+int cnt=0,a, b;
 int dx[8]={-1,1,0,0,-1,-1,1,1};
 int dy[8]={0,0,-1,1,-1,1,-1,1};
 vector<int> result;
 
 
 void DFS(int x, int y){
-    //cnt += 1;
     visit[x][y] = true;
     
     for(int i=0; i<8; i++){
         int next_X = x+dx[i];
         int next_Y = y+dy[i];
-        if(next_X<0 || b<=next_X || next_Y<0 || b<=next_Y) continue;
+        if(next_X<0 || b<=next_X || next_Y<0 || a<=next_Y) continue;
         if(arr[next_X][next_Y] == 1 && !visit[next_X][next_Y]){
             DFS(next_X, next_Y);
         }
@@ -43,16 +42,16 @@ int main(){
         for(int i=0; i<b; i++){
             for(int j=0; j<a; j++){
                 if(arr[i][j] == 1 && !visit[i][j]){
-                    //cnt = 0;
                     DFS(i, j);
-                    //result.push_back(cnt);
                     cnt++;
                 }
                 
             }
         }
         cout<<cnt<<'\n';
+        cnt=0;
         memset(visit, false, sizeof(visit));
+        memset(arr, 0, sizeof(arr));
     }
     
     return 0;
