b499d9c62a71af9f20250fbfdfb97cccd7108c17
강연욱 <rokkyw@khu.ac.kr>
2020-04-21 15:20:58 +0900

    쇠막대기_시뮬레이션_10799

---
+++ 기타/쇠막대기_시뮬레이션_10799/쇠막대기_시뮬레이션_10799/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+using namespace std;
+
+int solution(string arrangement) {
+    stack<char> s;
+    int cnt = 0;
+  
+    for(int j=0; j<arrangement.length(); j++){
+        if(arrangement[j] == '(' || s.empty()){
+            s.push(arrangement[j]);    //push하여 꾹꾹 눌러담음
+            //cout<<"size: "<<s.size()<<'\n';
+        }
+        else if(s.top() == '('){  //이부분 해석중요
+            s.pop();
+            if(arrangement[j-1]=='(')
+                cnt+=s.size();
+            else
+                cnt++;
+        }
+    }
+    return cnt;
+}
+// "() ( ( (() ()) (()) () ) ) (())"
+int main() {
+    string input;
+    cin>>input;
+    cout<<solution(input)<<'\n';
+    return 0;
+}
