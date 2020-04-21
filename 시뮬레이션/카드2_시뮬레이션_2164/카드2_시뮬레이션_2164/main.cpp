84c9ed384273e1e636c9af497381f69ac33f2d39
강연욱 <rokkyw@khu.ac.kr>
2020-04-21 15:22:38 +0900

    카드2_시뮬레이션_2164

---
+++ 시뮬레이션/카드2_시뮬레이션_2164/카드2_시뮬레이션_2164/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a; cin>>a;
+    queue<int> card;
+    for(int i=1; i<=a; i++){
+        card.push(i);
+    }
+    
+    while(card.size() != 1){
+        card.pop();
+        card.push(card.front());
+        card.pop();
+    }
+    cout<<card.front()<<'\n';
+    return 0;
+}
