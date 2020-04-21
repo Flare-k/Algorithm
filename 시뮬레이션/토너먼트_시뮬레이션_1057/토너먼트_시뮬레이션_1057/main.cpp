ceac31a8e53034d016f7e04ad91a7ffd1a6d2670
강연욱 <rokkyw@khu.ac.kr>
2020-04-21 15:22:54 +0900

    토너먼트_시뮬레이션_1057

---
+++ 시뮬레이션/토너먼트_시뮬레이션_1057/토너먼트_시뮬레이션_1057/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+/*
+ 
+ 1 2 /3 4/ 5 6/ 7 8 / 9 10/ 11 12/ 13 14/ 15 16
+          1 2 / 3 4 / 5 6 / 7 8
+                1 2 / 3 4
+                  1   2
+ 
+ */
+int main(){
+    int total, kim, imm;
+    cin>>total>>kim>>imm;
+    int cnt=0;
+    while(kim != imm){
+        kim = kim/2 + kim%2;
+        imm = imm/2 + imm%2;
+        cnt+=1;
+    }
+    cout<<cnt<<'\n';
+    return 0;
+}
