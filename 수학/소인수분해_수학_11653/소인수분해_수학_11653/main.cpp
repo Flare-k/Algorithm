7d8c0cbb93dc9232e59893d48e354657753b968c
Flare-k <rokkyw@naver.com>
2020-08-11 17:07:04 +0900

    BOJ 소인수분해 11653

---
+++ 수학/소인수분해_수학_11653/소인수분해_수학_11653/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#define endl "\n"
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    scanf("%d", &n);
+    
+    int div = 2;
+    
+    while(n != 1){
+        if(n%div == 0){
+            n /= div;
+            printf("%d\n", div);
+        }
+        else{
+            div++;
+        }
+    }
+    
+    return 0;
+}
+
