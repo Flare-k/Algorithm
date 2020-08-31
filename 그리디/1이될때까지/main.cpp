6f54c76334082cf3ec0f0b107282a41b92d891cc
Flare-k <rokkyw@naver.com>
2020-08-31 23:27:30 +0900

    1이될때까지 ndb Greedy

---
+++ 그리디/1이될때까지/main.cpp
@@ -0,0 +1,20 @@
+// 단순하게 푸는 경우
+#include <iostream>
+using namespace std;
+
+int main() {
+   int a, b;
+   cin >> a >> b;
+   int cnt = 0;
+   while (a != 1) {
+      if (a % b == 0) {
+         a /= b;
+      } else {
+         a -= 1;
+      }
+      cnt++;
+   }
+
+   cout << cnt << endl;
+   return 0;
+}
