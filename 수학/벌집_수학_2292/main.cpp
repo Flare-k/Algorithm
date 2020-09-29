372b0a36c2f51fe0b4bddba5f9fa9d3719ec7420
Flare-k <rokkyw@naver.com>
2020-09-29 22:29:21 +0900

    벌집_수학_2292

---
+++ 수학/벌집_수학_2292/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+/*
+1칸 -> 1 1개
+2칸 -> 2~7 6개
+3칸 -> 8~19 12개
+4칸 -> 20~37 18개
+
+1 (6) 7 (12) 19 (18) 37
+*/
+int main() {
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   cout.tie(NULL);
+
+   long long n;
+   cin >> n;  // 총 반복 횟수
+
+   long long cnt = 1;
+   long long number = 1;
+   long long mid = 1;
+
+   while (1) {
+      if (number >= n) break;
+      mid = 6 * (cnt++);
+      number += mid;
+   }
+   cout << cnt << endl;
+   return 0;
+}
