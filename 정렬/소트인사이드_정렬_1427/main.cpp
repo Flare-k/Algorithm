2a2f4ef63aea92aeeb78bb95df9ce5b745e0ea59
Flare-k <rokkyw@naver.com>
2020-10-06 20:47:57 +0900

    소트인사이드_정렬_1427

---
+++ 정렬/소트인사이드_정렬_1427/main.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#define endl "\n"
+using namespace std;
+
+int main() {
+   ios_base::sync_with_stdio(false);
+   cin.tie(0);
+   cout.tie(0);
+   string s;
+   cin >> s;
+   sort(s.rbegin(), s.rend());
+   cout << s;
+   return 0;
+}
