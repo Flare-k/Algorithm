4552ed1b01942d8ce18696924598fb61b811e2d1
Flare-k <rokkyw@naver.com>
2020-10-07 16:57:13 +0900

    지능형기차2_시뮬레이션_2460

---
+++ 시뮬레이션/지능형기차2_시뮬레이션_2460/main.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <queue>
+#include <utility>
+using namespace std;
+int main() {
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   cout.tie(NULL);
+   priority_queue<int> train;
+   int sum = 0;
+   for (int i = 0; i < 10; i++) {
+      int out, in;
+      cin >> out >> in;
+      sum -= out;
+      sum += in;
+      train.push(sum);
+   }
+   cout << train.top();
+   return 0;
+}
