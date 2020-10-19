f5741907001d0b072aa00db579fff4bcec833243
Flare-k <rokkyw@naver.com>
2020-10-19 17:31:42 +0900

    택시기하학_수학_3053

--- 수학/택시기하학/main.cpp
+++ 수학/택시기하학/main.cpp
@@ -1,10 +1,20 @@
+#include <cmath>
 #include <iostream>
+#define PI 3.1415926535897932
 using namespace std;
 
 int main() {
    ios_base::sync_with_stdio(false);
    cin.tie(NULL);
    cout.tie(NULL);
+
+   long double r;
+   cin >> r;
+
+   cout << fixed;
+   cout.precision(6);
+   cout << powl(r, 2) * PI << '\n';
+   cout << powl(r, 2) * 2 << '\n';
 
    return 0;
 }
