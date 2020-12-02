14e86577ac7d6251f678d3bdb62fae0e63a35e34
Flare-k <rokkyw@naver.com>
2020-12-03 00:16:23 +0900

    방문길이_시뮬레이션_Programmers

--- 시뮬레이션/방문길이_시뮬레이션_Prog/main.cpp
+++ 시뮬레이션/방문길이_시뮬레이션_Prog/main.cpp
@@ -16,18 +16,20 @@ int solution(string dirs) {
         case 'L':
             if (x > -5) x--; break;
         case 'R':
-           if (x < 5) x++; break;
+            if (x < 5) x++; break;
         case 'U':
-           if (y < 5) y++; break;
+            if (y < 5) y++; break;
         case 'D':
-           if (y > -5) y--; break;
+            if (y > -5) y--; break;
         }
         
         if (x == oldX && y == oldY) continue;
+        
         avatar.insert({oldX, oldY, x, y});
         avatar.insert({x, y, oldX, oldY});
         oldX = x;
         oldY = y;
     }
+    
     return avatar.size() / 2;
 }
