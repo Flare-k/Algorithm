520ab4f13a01473271d92538fe012b1d8d61ade1
Flare-k <rokkyw@naver.com>
2020-12-05 21:11:09 +0900

    Directory Update

--- 문자열/다이얼_문자열_5622.cpp
+++ 문자열/다이얼_문자열_5622.cpp
@@ -12,27 +12,27 @@ using namespace std;
 // 다이얼을 당기려면 문자열의 길이만큼 초가 더 추가된다.
 
 int main() {
-   ios_base::sync_with_stdio(false);
-   cin.tie(0);
-   cout.tie(0);
-   int dial[26] = {2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6,
-                   6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 9};
-   string word;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int dial[26] = {2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6,
+                    6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 9};
+    string word;
 
-   cin >> word;
-   int sum = 0;
-   for (int i = 0; i < word.length(); i++) {
-      if (word[i] == '1') {
-         sum += 1;
-      } else if (word[i] == '0') {
-         sum += 10;
-      } else
-         sum += dial[int(word[i]) - 65];
-   }
-   sum += word.length();
-   cout << sum;
+    cin >> word;
+    int sum = 0;
+    for (int i = 0; i < word.length(); i++) {
+        if (word[i] == '1') {
+            sum += 1;
+        } else if (word[i] == '0') {
+            sum += 10;
+        } else
+            sum += dial[int(word[i]) - 65];
+    }
+    sum += word.length();
+    cout << sum;
 
-   return 0;
+    return 0;
 }
 /*
 a = 97
