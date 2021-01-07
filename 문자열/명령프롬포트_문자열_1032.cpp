804fcea84b350bad4919d472f8de71d0f56f4e64
Flare-k <rokkyw@naver.com>
2021-01-07 22:42:39 +0900

    명령프롬포트_문자열_1032

---
+++ 문자열/명령프롬포트_문자열_1032.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin >> n;   // 문자열 개수
+
+    string cmd[n];
+
+    int i = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> cmd[i];
+    }
+
+    string res = "";    // 비교하기 위한 문자열 변수
+    res += cmd[0];      // 가장 첫 문자열을 res에 갱신
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < cmd[i].length(); j++) {
+            if (res[j] != cmd[i][j]) {
+                res[j] = '?';
+            }
+        }
+    }
+
+    cout << res;
+
+    return 0;
+}
