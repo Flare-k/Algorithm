1e6f37f4820b70efd6284083935733f8dffdd516
Flare-k <rokkyw@naver.com>
2021-02-24 21:30:29 +0900

    Init Project

---
+++ cloneApple/src/main/java/apple/cloneApple/CloneAppleApplication.java
@@ -0,0 +1,13 @@
+package apple.cloneApple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneAppleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneAppleApplication.class, args);
+	}
+
+}
