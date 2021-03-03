d63a19bf0eb4a88ff9802e0afb2016b58127f71e
rokkyw <rokkyw@naver.com>
2021-03-04 00:04:36 +0900

    Change Directory

---
+++ 자료구조/stack.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Stack{
+private:
+    int stack[10000];
+    int length = 0;
+public:
+    Stack(){}
+    ~Stack(){}
+    
+    void push(int data) {
+        stack[length] = data;
+        length++;
+    }
+
+    void pop() {
+        if(empty())
+            cout << -1 << '\n';
+        else {
+            cout << stack[length - 1] << '\n';
+            stack[length - 1] = 0;
+            length--;
+        }
+    }
+    
+    int size() {
+        return length;
+    }
+
+    int empty() {
+        if(length == 0)
+            return 1;
+        else
+            return 0;
+    }
+    
+    int top() {
+        if(empty())
+            return -1;
+        else
+            return stack[length - 1];
+    }
+
+};
+
+int main() {
+    int num;
+    string command;
+    int insert;
+    
+    cin >> num;
+      
+    Stack stack;
+    
+    for (int i = 0; i < num; i++) {
+        cin >> command;
+        
+        if (command == "push") {
+            cin >> insert;
+            stack.push(insert);
+        }
+        
+        if (command == "pop") {
+            stack.pop();
+        }
+
+        if (command == "size") {
+            cout << stack.size() << '\n';
+        }
+
+        if (command == "empty") {
+            cout << stack.empty() << '\n';
+        }
+
+        if (command == "top") {
+            cout << stack.top() << '\n';
+        }
+    }
+    
+    return 0;
+}
---
+++ 자료구조/큐_자료구조_10845.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+
+int main(){
+    int num;
+    cin >> num;
+
+    queue<int> q;
+
+    string command;
+    
+    for (int i = 0; i < num; i++) {
+        cin >> command;
+
+        if (command == "push") {
+            int random;
+            cin >> random;
+            q.push(random);
+        }
+
+        if (command == "front") {
+            if (q.size() != 0) {
+                cout << q.front() << '\n';
+            }
+            else
+                cout << -1 << '\n';
+        }
+        if (command == "pop") {
+            if (!q.empty()) {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+            else
+                cout << -1 << '\n';
+        }
+        if (command == "empty") {
+            if (q.empty())
+                cout << 1 << '\n';
+            else
+                cout << 0 << '\n';
+        }
+        if (command == "back") {
+            if (!q.empty())
+                cout << q.back() << '\n';
+            else
+                cout<<-1<<'\n';
+        }
+        if (command == "size") {
+            cout << q.size() << '\n';
+        }
+    }
+    
+    
+    return 0;
+}
