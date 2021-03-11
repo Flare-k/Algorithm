4fa912a002a772a39e73164475fb095550842bfa
rokkyw <rokkyw@naver.com>
2021-03-12 00:03:07 +0900

    Clean Code

--- TwoPointer/twoPointer_개념.cpp
+++ TwoPointer/twoPointer_개념.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 int main(){
-    int n=5;
-    int m=5;
-    int data[5]={1,2,3,2,3};
+    int n = 5, m = 5;
+    int data[5] = {1, 2, 3, 2, 3};
     
-    int result = 0;
-    int summary = 0;
-    int end = 0;
+    int result = 0, summary = 0, end = 0;
     
-    for(int start=0; start<n; start++){
-        while(summary < m && end < n){
+    for (int start = 0; start < n; start++) {
+        while (summary < m && end < n) {
             summary += data[end];
             end++;
         }
-        if(summary == m){
+
+        if (summary == m){
             result++;
         }
+
         summary -= data[start];
     }
-    cout<<"Result: "<<result<<'\n';
+
+    cout << "Result: " << result << '\n';
     
     return 0;
 }
