7cd609310d967799714b11a26333ee2b5a40c089
rokkyw <rokkyw@naver.com>
2021-03-17 22:26:19 +0900

    이미지 돌리기

--- 기본기/삽입정렬,구현/이미지돌리기.cpp
+++ 기본기/삽입정렬,구현/이미지돌리기.cpp
@@ -7,6 +7,7 @@ N, K를 입력받고 NxN인 이미지를 만든다.
 입력 받은 K 만큼 오른쪽으로 돌린 결과 나타내기
 */
 
+// 이미지를 오른쪽으로 돌려 tmp에 저장하고 다시 board에 갱신
 void turnRight(int N, vector<vector<int>>& board) {
     vector<vector<int>> tmp(N, vector<int>(N));
 
@@ -29,16 +30,19 @@ int main() {
 
     vector<vector<int>> board(N, vector<int>(N));
 
+    // 이미지 입력
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             cin >> board[i][j];
         }
     }
 
+    // 이미지 돌리기
     while (K--) {
         turnRight(N, board);
     }
 
+    // 이미지 출력
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             cout << board[i][j] << ' ';
