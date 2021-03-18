77e26b1392fba0c262c21b6fc55fa1a42c772bfb
rokkyw <rokkyw@naver.com>
2021-03-18 22:42:48 +0900

    Union Find 경로압축

--- 기본기/unionFind/unionFind.cpp
+++ 기본기/unionFind/unionFind.cpp
@@ -3,9 +3,18 @@ using namespace std;
 
 int vect[100];
 
+// getBoss가 리턴되면서 보스를 변경.
+/*
+그렇게 된다면
+a <- b <- c <- d d의 보스가 a임을 확인할 때 계속 재귀를 했어야 했는데
+a <- b, a <- b, a <- d 로 더 간결하게 접근이 가능해진다.
+*/
 char getBoss(char ch) {
     if (vect[ch] == 0) return ch;
+
     char res = getBoss(vect[ch]);
+
+    vect[ch] = res; // 이 부분을 추가해줌으로써 추후에 getBoss 호출시 이전보다 속도가 더 빠르다. (= 경로 압축)
 
     return res;
 }
