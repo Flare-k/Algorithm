15261965792b61e21265fbb25137ab8d734e376f
rokkyw <rokkyw@naver.com>
2021-03-28 23:40:04 +0900

    Clean Code

--- 정렬/H-Index_정렬_Prog.cpp
+++ 정렬/H-Index_정렬_Prog.cpp
@@ -7,8 +7,9 @@ int solution(vector<int> citations) {
     int answer = 0;
    
     sort(citations.begin(), citations.end(), greater<int>());
-    for(int i=0; i<citations.size(); i++){
-        if(citations[i] <= answer)
+
+    for (int i = 0; i < citations.size(); i++) {
+        if (citations[i] <= answer)
             break;
         
         answer++;
@@ -16,9 +17,11 @@ int solution(vector<int> citations) {
     
     return answer;
 }
+
 int main(){
-    vector<int> citations = {3,0,6,1,5};
-    cout<<solution(citations)<<'\n';
+
+    vector<int> citations = {3, 0, 6, 1, 5};
+    cout << solution(citations) << '\n';
     
     return 0;
 }
