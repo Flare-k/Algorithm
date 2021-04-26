3370206fc33db7adcaacee9dfdee14e5ac80662e
rokkyw <rokkyw@naver.com>
2021-04-27 00:20:19 +0900

    좌표압축_정렬_18870

---
+++ 정렬/좌표압축_정렬_18870.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 1000001;
+int n;
+
+struct Node {
+    int num;
+    int rank;
+};
+
+bool cmp(Node a, Node b) {
+    if (a.num < b.num) return true;
+    return false;
+}
+
+int main() {
+    cin >> n;
+
+    vector<Node> arr;
+    int a;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        arr.push_back({a, 0});
+    }
+
+    vector<Node> tmp;
+    tmp.clear();
+    tmp.assign(arr.begin(), arr.end());
+
+    sort(tmp.begin(), tmp.end(), cmp);
+    map<int, int> val;
+
+    tmp[0].rank = 0;
+    val[tmp[0].num] = 0;
+    for (int i = 1; i < tmp.size(); i++) {
+        if (tmp[i].num == tmp[i - 1].num) {
+            tmp[i].rank = tmp[i - 1].rank;
+            continue;
+        }
+        tmp[i].rank = tmp[i - 1].rank + 1;
+        val[tmp[i].num] = tmp[i - 1].rank + 1;
+    }
+
+    for (int i = 0; i < arr.size(); i++) {
+        cout << val[arr[i].num] << ' ';
+    }
+
+    return 0;
+}
