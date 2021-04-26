f988ef13bd8b0cb46980e3b52d0d1f236e7cea6a
rokkyw <rokkyw@naver.com>
2021-04-26 23:24:45 +0900

    그래프/경로찾기_그래프_11403

---
+++ 그래프/경로찾기_그래프_11403.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+const int MAX = 100;
+int arr[MAX][MAX];
+bool visit[MAX][MAX];
+int n;
+
+void floyd() {
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i][k] && arr[k][j]) arr[i][j] = 1;
+            }
+        }
+    }   
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    floyd();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << arr[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
