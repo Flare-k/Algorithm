11a29e0fd5fed956e88c57fc5b850ee91de5cb6f
rokkyw <rokkyw@naver.com>
2021-04-28 11:12:44 +0900

    두수의합_twoPointer_3273

---
+++ TwoPointer/두수의합_twoPointer_3273.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    sort(arr.begin(), arr.end());
+
+    int target;
+    cin >> target;
+
+    int start = 0;
+    int end = arr.size() - 1;
+
+    int sum = 0;
+    int cnt = 0;
+
+    while (1) {
+        if (start >= end) break;
+
+        sum = arr[start] + arr[end];
+
+        if (sum < target) {
+            start++;
+        }
+        else if (sum > target) {
+            end--;
+        }
+        else if (sum == target) {
+            cnt++;
+            end--;
+            start++;
+        }
+
+    }
+
+    cout << cnt;
+
+
+    return 0;
+}
