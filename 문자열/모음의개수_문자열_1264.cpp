7ac9c75df0a15be245502ad246e0772693eb9339
rokkyw <rokkyw@naver.com>
2021-06-08 02:20:01 +0900

    문자열/모음의개수_문자열_1264.cpp

---
+++ 문자열/모음의개수_문자열_1264.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    while (1) {
+        string str;
+        getline(cin, str);
+
+        if (str == "#") break;
+
+        int cnt = 0;
+
+        for (char s : str) {
+            if (s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u'
+            || s == 'A' || s == 'E' || s == 'I' || s == 'O' || s == 'U') {
+                cnt++;
+            }
+        }
+
+        cout << cnt << '\n';
+    }
+
+    return 0;
+}
