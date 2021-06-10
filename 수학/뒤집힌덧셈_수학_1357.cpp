a013f007d5b10f813a993dbeb40dd31a96cdecdf
rokkyw <rokkyw@naver.com>
2021-06-10 14:10:38 +0900

    수학/뒤집힌덧셈_수학_1357.cpp

---
+++ 수학/뒤집힌덧셈_수학_1357.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string left, right;
+    cin >> left >> right;
+
+    reverse(left.begin(), left.end());
+    reverse(right.begin(), right.end());
+
+    string ans = to_string(stoi(left) + stoi(right));
+    reverse(ans.begin(), ans.end());
+
+    cout << ans;
+
+    return 0;
+}
