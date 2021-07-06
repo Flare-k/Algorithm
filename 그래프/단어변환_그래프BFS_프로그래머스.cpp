1848274cc5d93ac467851eaded03c1480682d3dd
rokkyw <rokkyw@naver.com>
2021-07-07 01:30:01 +0900

    그래프/단어변환_그래프BFS_프로그래머스.cpp

--- 그래프/단어변환_그래프BFS_프로그래머스.cpp
+++ 그래프/단어변환_그래프BFS_프로그래머스.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <queue>
 #include <vector>
 using namespace std;
 
 int result = 21e8;
 bool used[51];
+
+struct Node {
+    string word;
+    int level;
+};
 
 int checkStr(string a, string b) {
     int cnt = 0;
@@ -17,32 +23,32 @@ int checkStr(string a, string b) {
     return cnt;
 }
 
-void run(string begin, string target, vector<string>& words, int level) {
-    if (begin == target) {
-        result = min(result, level);
-        return;
-    }
-
-    for (int i = 0; i < words.size(); i++) {
-        int dif = checkStr(begin, words[i]);
-
-        if (dif == 1) {
-            if (!used[i]) {
-                used[i] = true;
-                run(words[i], target, words, level + 1);
-                used[i] = false;
-            }
-        }
-    }
-}
-
 int solution(string begin, string target, vector<string> words) {
     int answer = 0;
 
-    run(begin, target, words, 0);
+    queue<Node> q;
 
-    if (result != 21e8) answer = result;
-    else answer = 0;
+    q.push({begin, 0});
+    int idx = 0;
+
+    while (!q.empty()) {
+        Node now = q.front();
+        q.pop();
+
+        if (now.word == target) {
+            answer = now.level;
+            break;
+        }
+
+        for (int i = 0; i < words.size(); i++) {
+            if (used[i]) continue;
+            if (checkStr(now.word, words[i]) == 1) {
+                used[i] = true;
+                q.push({words[i], now.level + 1});
+            }
+
+        }
+    }
 
     return answer;
 }
