eb37cb6f1a2195a9a2365fa6f14de22279b5f684
rokkyw <rokkyw@naver.com>
2021-07-09 19:23:35 +0900

    수학/예상대진표_수학_프로그래머스.cpp

---
+++ 수학/예상대진표_수학_프로그래머스.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n, int a, int b) {
+    int answer = 0;
+    
+    while (a != b) {
+        a = (a / 2) + (a % 2);
+        b = (b / 2) + (b % 2);
+        answer++;
+    }
+
+    return answer;
+}
+
+int main() {
+
+    cout << solution(8, 4, 7) << '\n';
+    return 0;
+}
