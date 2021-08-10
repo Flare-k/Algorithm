25190b41e280c5b51a3d49c6a26a8ec029cd5a5c
rokkyw <rokkyw@naver.com>
2021-08-10 16:09:37 +0900

    문자열/KMP_Algorithm.cpp 주석 추가

--- 문자열/KMP_Algorithm.cpp
+++ 문자열/KMP_Algorithm.cpp
@@ -9,6 +9,9 @@ using namespace std;
  * 
  */
 
+// 각 위치에서 접두사와 접미사 간의 일치하는 길이만큼 테이블 갱신
+// a b a c a a b a
+// 0 0 1 0 1 1 2 3
 vector<int> makeTable(string pattern) {
     int patternSize = pattern.size();
     vector<int> table(patternSize, 0);
@@ -34,6 +37,7 @@ void KMP(string parent, string pattern) {
     int patternSize = pattern.size();
 
     int j = 0;
+    
     for (int i = 0; i < parentSize; i++) {
         while (j > 0 && parent[i] != pattern[j]) {
             j = table[j - 1];
@@ -55,7 +59,7 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    string parent = "ababacaabaabacaaba";
+    string parent = "abacaabaabacaaba";
     string pattern = "abacaaba";
 
     vector<int> table = makeTable(pattern);
