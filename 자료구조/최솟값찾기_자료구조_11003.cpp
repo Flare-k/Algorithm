76ff95da77e05b170f7e6b93c85c08a1c1309639
rokkyw <rokkyw@naver.com>
2021-08-14 23:36:54 +0900

    자료구조/최솟값찾기_자료구조_11003.cpp

---
+++ 자료구조/최솟값찾기_자료구조_11003.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <deque>
+using namespace std;
+
+struct Node {
+    int value, idx;
+};
+
+deque<Node> dq;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, L;
+    cin >> N >> L;
+
+    vector<int> arr(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (!dq.empty() && dq.front().idx <= i - L) {
+            dq.pop_front();
+        }
+
+        while (!dq.empty() && dq.back().value > arr[i]) {
+            dq.pop_back();
+        }
+
+        dq.push_back({arr[i], i});
+        cout << dq.front().value << ' ';
+    }
+
+    return 0;
+}
