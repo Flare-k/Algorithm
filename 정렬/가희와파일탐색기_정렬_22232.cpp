8948314ec325ac1b7732e5124d1ad313eada2090
rokkyw <rokkyw@naver.com>
2021-10-04 00:21:20 +0900

    정렬/가희와파일탐색기_정렬_22232.cpp

--- 정렬/가희와파일탐색기_정렬_22232.cpp
+++ 정렬/가희와파일탐색기_정렬_22232.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
 
 struct Node {
@@ -11,10 +12,38 @@ struct Node {
 int N, M;
 vector<Node> v;
 vector<string> filenames;
-vector<string> extensions;
+unordered_map<string, bool> e;
+
+bool cmp(Node a, Node b) {
+    if (a.filename < b.filename) return true;
+    else if (a.filename == b.filename) {
+        if (e[a.extension] == e[b.extension]) return true;
+        else if (e[a.extension] == false && e[b.extension] == true) return true;
+        else if (e[a.extension] == false && e[b.extension] == false) return a.extension < b.extension;
+        else return a.extension < b.extension;
+    }
+    return false;
+}
 
 void split() {
+    for (string filename : filenames) {
+        int left = 0, right = 0;
+        Node now;
 
+        while (1) {
+
+            right = filename.find('.', left);
+
+            if (right == -1) {
+                now.extension = filename.substr(left);
+                v.push_back(now);
+                break;
+            }
+
+            now.filename = filename.substr(left, right - left);
+            left = right + 1;
+        }
+    }
 }
 
 int main() {
@@ -31,9 +60,16 @@ int main() {
 
     for (int i = 0; i < M; i++) {
         cin >> s;
-        extensions.push_back(s);
+        e[s] = true;
     }
 
+    split();
+
+    sort(v.begin(), v.end(), cmp);
+
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i].filename << '.' << v[i].extension << '\n';
+    }
 
     return 0;
 }
