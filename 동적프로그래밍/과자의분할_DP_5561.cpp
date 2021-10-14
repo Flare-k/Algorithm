75f60c0d0be46446d79e0f0db460c7e348d1adab
rokkyw <rokkyw@naver.com>
2021-10-15 00:36:07 +0900

    동적프로그래밍/과자의분할_DP_5561.cpp *****

--- 동적프로그래밍/과자의분할_DP_5561.cpp
+++ 동적프로그래밍/과자의분할_DP_5561.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 #include <cstring>
 using namespace std;
 
-const int MAX = 10001;
-int N;
 
-int dp1[MAX];
-int dp2[MAX];
+int N;
+// dp[n][m]: 1...n 과자 중 n을 포함해 m개 가지기 위해 필요한 최소 힘
+vector<int> dp[2];
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -15,21 +15,23 @@ int main() {
 
     cin >> N;
 
+    dp[0].resize(N);
+    dp[1].resize(N);
+
     int num;
     for (int i = 1; i < N; i++) {
         cin >> num;
+
         for (int j = i; j > 1; j--) {
-            int a = dp1[j-1];
-            int b = dp1[i+1-j] + num;
-            dp2[j] = min(dp1[j-1], dp1[i-(j-1)] + num);
+            dp[1][j] = min(dp[0][j-1], dp[0][i-(j-1)] + num);
         }
 
-        dp2[1] = num;
-        copy(dp2, dp2 + 1 + i, dp1);
+        dp[1][1] = num;
+        copy(dp[1].begin(), dp[1].begin() + 1 + i, dp[0].begin());
         
     }
    
-    cout << dp1[N / 2];
+    cout << dp[0][N / 2];
 
     return 0;
 }
