1df40030cef6eff03d6027c419a15607f9bab1f6
rokkyw <rokkyw@naver.com>
2021-10-18 17:51:36 +0900

    동적프로그래밍/돌게임_DP_9655.cpp - DP로 풀이

--- 동적프로그래밍/돌게임_DP_9655.cpp
+++ 동적프로그래밍/돌게임_DP_9655.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
+// DP로 풀이
+int N;
+int dp[1001];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> N;
+
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 1;
+
+    for (int i = 4; i <= N; i++) {
+        dp[i] = min(dp[i-3] + 1, dp[i-1] + 1);
+    }
+
+    if (dp[N] % 2) cout << "SK";
+    else cout << "CY";
+
+    return 0;
+}
+
+/*
 // 수식으로 풀이
 int main() {
     ios_base::sync_with_stdio(false);
@@ -13,3 +38,4 @@ int main() {
 
     return 0;
 }
+*/
