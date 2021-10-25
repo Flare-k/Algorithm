e9d3e8d39752c2aa0510fbfd11bd1aa23b1f2f47
rokkyw <rokkyw@naver.com>
2021-10-26 01:00:00 +0900

    이진탐색/수찾기_이분탐색_1920.cpp

--- 이진탐색/수찾기_이분탐색_1920.cpp
+++ 이진탐색/수찾기_이분탐색_1920.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstring>
 #include <iostream>
 #define endl "\n"
 using namespace std;
@@ -8,45 +9,53 @@ int n, m;
 int arr[MAX];
 
 int binarySearch(int num) {
-   int first = 0;
-   int last = n - 1;
-   int mid = 0;
+    int first = 0;
+    int last = n - 1;
+    int mid = 0;
 
-   while (first <= last) {
-      mid = (first + last) / 2;
+    while (first <= last) {
+        mid = (first + last) / 2;
 
-      if (num == arr[mid])
-         return 1;
-      else {
-         if (num < arr[mid])
-            last = mid - 1;
-         else
-            first = mid + 1;
-      }
-   }
-   return 0;
+        if (num == arr[mid]) {
+            return 1;
+        }    
+        else {
+            if (num < arr[mid]) last = mid - 1;
+            else first = mid + 1;
+        }
+    }
+
+    return 0;
 }
 
 void init() {
-   ios_base::sync_with_stdio(false);
-   cin.tie(NULL);
-   cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 }
+
 int main() {
-   init();
-   cin >> n;
-   for (int i = 0; i < MAX; i++) {
-      arr[i] = 0;
-   }
-   for (int i = 0; i < n; i++) {
-      cin >> arr[i];
-   }
-   sort(arr, arr + n);
-   cin >> m;
-   int num;
-   for (int i = 0; i < m; i++) {
-      cin >> num;
-      cout << binarySearch(num) << endl;
-   }
-   return 0;
+    
+    init();
+
+    cin >> n;
+
+    memset(arr, 0, sizeof(arr));
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    cin >> m;
+
+    int num;
+
+    for (int i = 0; i < m; i++) {
+        cin >> num;
+        cout << binarySearch(num) << endl;
+    }
+    
+    return 0;
 }
