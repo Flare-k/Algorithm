6c078cdbde97370de562bcc1390ca1991a1dc75f
rokkyw <rokkyw@naver.com>
2021-11-02 22:21:20 +0900

    위상정렬/문제집_위상정렬_1766.cpp

--- 위상정렬/문제집_위상정렬_1766.cpp
+++ 위상정렬/문제집_위상정렬_1766.cpp
@@ -11,33 +11,33 @@ int n, m, inDegree[MAX];
 vector<int> arr[MAX];
 
 void topologySort() {
-   priority_queue<int, vector<int>, greater<int>> q;    // 우선순위 큐(오름차순)
-   int result[MAX];
-   memset(result, 0, sizeof(result));
-   
-   for (int i = 1; i <= n; i++) {
-      if (inDegree[i] == 0) q.push(i);
-   }
-   
-   for (int i = 1; i <= n; i++) {
-      if (q.empty()) return;
+    priority_queue<int, vector<int>, greater<int>> q;    // 우선순위 큐(오름차순)
+    int result[MAX];
+    memset(result, 0, sizeof(result));
+    
+    for (int i = 1; i <= n; i++) {
+        if (inDegree[i] == 0) q.push(i);
+    }
+    
+    for (int i = 1; i <= n; i++) {
+        if (q.empty()) return;
 
-      int x = q.top();
-      q.pop();
+        int x = q.top();
+        q.pop();
 
-      result[i] = x;
+        result[i] = x;
 
-      for (int j = 0; j < arr[x].size(); j++) {
-         int y = arr[x][j];
-         if (--inDegree[y] == 0) {
-            q.push(y);
-         }
-      }
-   }
+        for (int j = 0; j < arr[x].size(); j++) {
+            int y = arr[x][j];
+            if (--inDegree[y] == 0) {
+                q.push(y);
+            }
+        }
+    }
 
-   for (int i = 1; i <= n; i++) {
-      cout << result[i] << " ";
-   }
+    for (int i = 1; i <= n; i++) {
+        cout << result[i] << " ";
+    }
 
 }
 
