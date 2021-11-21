d50c6bbb4beb1ec8cc294c61607a11e0e9ccdf32
rokkyw <rokkyw@naver.com>
2021-11-21 22:45:23 +0900

    자료구조/기능개발_자료구조_프로그래머스.cpp

--- 자료구조/기능개발_자료구조_프로그래머스.cpp
+++ 자료구조/기능개발_자료구조_프로그래머스.cpp
@@ -12,13 +12,10 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
     for (int i = 0; i < progresses.size(); ++i) {
         day = (99 - progresses[i]) / speeds[i] + 1;
 
-        if (answer.empty() || max_day < day)
-            answer.push_back(1);
-        else
-            ++answer.back();
+        if (answer.empty() || max_day < day) answer.push_back(1);
+        else ++answer.back();
 
-        if (max_day < day)
-            max_day = day;
+        if (max_day < day) max_day = day;
     }
 
     return answer;
@@ -28,10 +25,14 @@ int main() {
     vector<int> progresses{93, 30, 55};
     vector<int> speeds{1, 30, 5};
     
-    for (int i = 0; i < solution(progresses, speeds).size(); i++) {
-        cout << solution(progresses, speeds)[i] << ' ';
+    vector<int> sol = solution(progresses, speeds);
+    int size = sol.size();
+
+    for (int i = 0; i < size; i++) {
+        cout << sol[i] << ' ';
     }
 
     cout << '\n';
+
     return 0;
 }
