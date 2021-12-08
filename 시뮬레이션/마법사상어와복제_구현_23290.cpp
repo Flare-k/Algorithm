02b72ee6bf1eb35967548d2cf4b1202964830542
rokkyw <rokkyw@naver.com>
2021-12-09 02:17:03 +0900

    시뮬레이션/마법사상어와복제_구현_23290.cpp

--- 시뮬레이션/마법사상어와복제_구현_23290.cpp
+++ 시뮬레이션/마법사상어와복제_구현_23290.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <queue>
 #include <string>
+#include <cstring>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 const int dx[] = {0, -1, -1, -1, 0, 1, 1, 1};
@@ -10,162 +12,144 @@ const int sx[] = {-1, 0, 1, 0};
 const int sy[] = {0, -1, 0, 1};
 const int MAX = 4;
 int arr[MAX][MAX];
-bool smell[MAX][MAX];
-int smellCnt[MAX][MAX];
+int smell[MAX][MAX];
+bool used[5];
 int M, S;
 
 struct Fish {
     int x, y, dir;
 };
 
-struct Dir {
-    int px, py, x, y, val;
-    string path;
-};
-
 Fish shark;
-queue<Fish> fish;
+vector<string> mc;
 
-void sharkMove() {
+void moveCase(int level, string path) {
+    if (level == 3) {
+        mc.push_back(path);
+        return;
+    }
+
+    for (int i = 1; i <= 4; i++) {
+        moveCase(level + 1, path + to_string(i));
+    }
+}
+
+queue<Fish> sharkMove(queue<Fish>& fish, int time) {
     int x = shark.x;
     int y = shark.y;
-    queue<Dir> q;
-    q.push({x, y, x, y, 0, ""});
 
-    string target = "444";
-    int cnt = -21e8;
-    int nx, ny;
+    string target;
+    int max_eat = -21e8;
+    
+    bool visit[MAX][MAX];
 
-    while (!q.empty()) {
-        Dir now = q.front();
-        q.pop();
+    for (auto dir : mc) {
+        memset(visit, false, sizeof(visit));
+        int s_x = x;
+        int s_y = y;
+        int eat = 0;
+        bool flag = false;
 
-        for (int i = 0; i < 4; i++) {
-            nx = now.x + sx[i];
-            ny = now.y + sy[i];
+        for (int i = 0; i < dir.length(); i++) {
+            int idx = (dir[i] - '0') - 1;
+            s_x += sx[idx];
+            s_y += sy[idx];
 
-            if (now.px == nx && now.py == ny) continue; // 바로 직전 좌표와 같은 경우
-            if (nx < 0 || ny < 0 || nx >= MAX || ny >= MAX) continue;
-
-            if (now.path.length() == 3) {
-                if (cnt <= now.val) {
-                    if (cnt == now.val) {
-                        if (target > now.path) target = now.path;
-                    }
-                    else if (cnt < now.val) target = now.path;
-                    
-                    cnt = now.val;
-                }
-                continue;
+            if (s_x < 0 || s_y < 0 || s_x >= MAX || s_y >= MAX) {
+                flag = false;
+                break;
             }
+            if (!visit[s_x][s_y]) {
+               visit[s_x][s_y] = true;
+               eat += arr[s_x][s_y];
+               flag = true;
+            }
+        }
 
-            q.push({now.x, now.y, nx, ny, now.val + arr[nx][ny], now.path + to_string(i + 1)});
+        if (max_eat < eat && flag) {
+            max_eat = eat;
+            target = dir;
         }
     }
 
-    if (cnt > 0){
-        for (char ch : target) {
-            int i = (ch - '0') - 1;
-            nx = x + sx[i];
-            ny = y + sy[i];
+    for (char ch : target) {
+        int i = (ch - '0') - 1;
+        x += sx[i];
+        y += sy[i];
 
-            if (arr[nx][ny] > 0) {
-                arr[nx][ny] = 0;
-                smell[nx][ny] = true;
-                smellCnt[nx][ny] = 2;
-            }
-            x = nx;
-            y = ny;
-        }
-
-        shark = {nx, ny};
-
-        int size = fish.size();
-        while (size--) {
-            Fish now = fish.front();
-            fish.pop();
-            if (arr[now.x][now.y] == 0) continue;
-            fish.push(now);
+        if (arr[x][y] != 0) {
+            arr[x][y] = 0;
+            smell[x][y] = time;
         }
     }
+
+    shark = {x, y};
+
+    int size = fish.size();
+
+    while (size--) {
+        Fish now = fish.front();
+        fish.pop();
+        if (arr[now.x][now.y] == 0) continue;
+        fish.push(now);
+    }
+
+    return fish;
 }
 
-void removeSmell() {
-    for (int i = 0; i < MAX; i++) {
-        for (int j = 0; j < MAX; j++) {
-            if (smellCnt[i][j] > 0) {
-                smellCnt[i][j]--;
-                if (smellCnt[i][j] == 0) smell[i][j] = false;
-            }
-        }
-    }
-}
-
-bool canNotMove(int x, int y) {
-    if (smell[x][y] == true) return true;
+bool canNotMove(int x, int y, int time) {
+    if (smell[x][y] != 0 && time - smell[x][y] <= 2) return true;
     if (shark.x == x && shark.y == y) return true;
     if (x < 0 || y < 0 || x >= MAX || y >= MAX) return true;
     return false;
 }
 
-void run() {
-    while (S--) {
-        queue<Fish> nq;
-        int size = fish.size();
+void run(queue<Fish>& fish, int time) {
+    queue<Fish> nq;
+    int size = fish.size();
 
-        while (size--) {
-            Fish now = fish.front();
-            fish.pop();
+    while (size--) {
+        Fish now = fish.front();
+        fish.pop();
 
-            int x = now.x;
-            int y = now.y;
-            int dir = now.dir;
-            int odir = now.dir;
+        int x = now.x;
+        int y = now.y;
+        int dir = now.dir;
 
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-            bool flag = false;
+        int nx, ny, ndir;
+        bool flag = false;
 
-            while (canNotMove(nx, ny)) {
-                dir--;
-                if (dir < 0) dir = 7;
-                if (dir == odir) {
-                    nx = x;
-                    ny = y;
-                    flag = true;
-                    break;
-                }
-                nx = x + dx[dir];
-                ny = y + dy[dir];
-            }
+        for (int i = 0; i < 8; i++) {
+            ndir = (8 + dir - i) % 8;
+            nx = x + dx[ndir];
+            ny = y + dy[ndir];
 
-            if (!flag) {
-                arr[x][y]--;
-                arr[nx][ny]++;
-            }
+            if (canNotMove(nx, ny, time)) continue;
 
-            fish.push({nx, ny, dir});
-            nq.push(now);
+            flag = true;
+            arr[x][y]--;
+            arr[nx][ny]++;
+            fish.push({nx, ny, ndir});
+
+            break;
         }
 
-        if (S == 0) {
-            int a = 1;
+        if (!flag) {
+            fish.push({x, y, dir});
         }
 
-        removeSmell();
-        sharkMove();
-
-        // 복제 마법 완료
-        while (!nq.empty()) {
-            Fish now = nq.front();
-            fish.push(now);
-            arr[now.x][now.y]++;
-            nq.pop();
-        }
-
+        nq.push(now);
     }
 
-    cout << fish.size();
+    fish = sharkMove(fish, time);
+
+    // 복제 마법 완료
+    while (!nq.empty()) {
+        Fish now = nq.front();
+        fish.push(now);
+        arr[now.x][now.y]++;
+        nq.pop();
+    }
 }
 
 int main() {
@@ -174,7 +158,9 @@ int main() {
 
     cin >> M >> S;
 
+    queue<Fish> fish;
     int x, y, dir;
+
     for (int i = 0; i < M; i++) {
         cin >> x >> y >> dir;
         fish.push({x - 1, y - 1, dir - 1});
@@ -184,7 +170,14 @@ int main() {
     cin >> x >> y;
 
     shark = {x - 1, y - 1};
-    run();
+
+    moveCase(0, "");
+
+    for (int time = 1; time <= S; time++) {
+        run(fish, time);
+    }
+
+    cout << fish.size();
 
     return 0;
 }
