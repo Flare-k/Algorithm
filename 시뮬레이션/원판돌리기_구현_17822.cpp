ec915aebaaafcb67515029b08f3a3efe1b190cfd
rokkyw <rokkyw@naver.com>
2021-12-14 01:01:26 +0900

    시뮬레이션/원판돌리기_구현_17822.cpp

--- 시뮬레이션/원판돌리기_구현_17822.cpp
+++ 시뮬레이션/원판돌리기_구현_17822.cpp
@@ -16,7 +16,6 @@ struct Node {
 
     void shuffle(int dir, int k) {
         while (k--) {
-            
             if (dir == CLOCK) {
                 int num = dq.back();
                 dq.pop_back();
@@ -34,49 +33,100 @@ struct Node {
 vector<Node> v;
 
 void checkAdj() {
-    memset(visit, false, sizeof(visit));
-
+    int cnt = 0;
     for (int i = 1; i <= N; i++) {
         for (int j = 0; j < M; j++) {
             if (v[i].dq[j] == 0) continue;
+            
             // 좌우
             if (j > 0 && j < M - 1) {
                 if (v[i].dq[j] == v[i].dq[j - 1]) {
                     visit[i][j] = true;
                     visit[i][j - 1] = true;
+                    cnt++;
                 }
                 if (v[i].dq[j] == v[i].dq[j + 1]) {
                     visit[i][j] = true;
                     visit[i][j + 1] = true;
+                    cnt++;
                 }
             }
             else if (j == 0) {
                 if (v[i].dq[j] == v[i].dq[M - 1]) {
                     visit[i][j] = true;
                     visit[i][M - 1] = true;
+                    cnt++;
                 }
-                if (v[i].dq[j] == v[i].dq[j + 1]) {
+                if (v[i].dq[j] == v[i].dq[1]) {
                     visit[i][j] = true;
-                    visit[i][j + 1] = true;
+                    visit[i][1] = true;
+                    cnt++;
                 }
             }
             else if (j == M - 1) {
                 if (v[i].dq[j] == v[i].dq[j - 1]) {
                     visit[i][j] = true;
                     visit[i][j - 1] = true;
+                    cnt++;
                 }
                 if (v[i].dq[j] == v[i].dq[0]) {
                     visit[i][j] = true;
                     visit[i][0] = true;
+                    cnt++;
                 }
             }
 
             // 다음 원판
-            if (i != N) {
+            if (i > 1 && i < N) {
+                if (v[i].dq[j] == v[i - 1].dq[j]) {
+                    visit[i][j] = true;
+                    visit[i - 1][j] = true;
+                    cnt++;
+                }
                 if (v[i].dq[j] == v[i + 1].dq[j]) {
                     visit[i][j] = true;
                     visit[i + 1][j] = true;
+                    cnt++;
                 }
+            }
+            else if (i == 1) {
+                if (v[i].dq[j] == v[2].dq[j]) {
+                    visit[i][j] = true;
+                    visit[2][j] = true;
+                    cnt++;
+                }
+            }
+            else if (i == N) {
+                if (v[i].dq[j] == v[N - 1].dq[j]) {
+                    visit[i][j] = true;
+                    visit[N - 1][j] = true;
+                    cnt++;
+                }
+            }
+        }
+    }
+
+    if (cnt == 0) {
+        int sum = 0, t = 0;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (v[i].dq[j] != 0) {
+                    sum += v[i].dq[j];
+                    t++;
+                }
+            }
+        }
+
+        double avg = double(sum) / double(t);
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (v[i].dq[j] == 0) continue;
+                
+                if (v[i].dq[j] == avg) continue;
+                else if (v[i].dq[j] > avg) v[i].dq[j]--;
+                else if (v[i].dq[j] < avg) v[i].dq[j]++;
             }
         }
     }
@@ -85,11 +135,11 @@ void checkAdj() {
 void clearAdj() {
     for (int i = 1; i <= N; i++) {
         for (int j = 0; j < M; j++) {
-            if (v[i].dq[j] != 0 && visit[i][j]) {
-                v[i].dq[j] = 0;
-            }
+            if (visit[i][j] == true) v[i].dq[j] = 0;
         }
     }
+
+    memset(visit, false, sizeof(visit));
 }
 
 int main() {
@@ -101,7 +151,7 @@ int main() {
 
     int num;
     for (int i = 1; i <= N; i++) {
-        for (int j = 0; j < 4; j++) {
+        for (int j = 0; j < M; j++) {
             cin >> num;
             v[i].dq.push_back(num);
         }
@@ -111,30 +161,10 @@ int main() {
     for (int t = 0; t < T; t++) {
         // x의 배수인 원판을 d방향으로 k칸 회전
         cin >> x >> d >> k;
+        
         // 회전 후, 인접한 원판 확인
-
-        // cout << '\n';
-        // for (int a = 1; a <= N; a++) {
-        //     for (int b = 0; b < M; b++) {
-        //         cout << v[a].dq[b];
-        //     }
-        //     cout << '\n';
-        // }
-        for (int i = 1; i <= N; i++) {
-            if (i % x == 0) {
-                v[i].shuffle(d, k);
-            }
-        }
-
-        if (t == 2) {
-            cout << '\n';
-            for (int a = 1; a <= N; a++) {
-                for (int b = 0; b < M; b++) {
-                    cout << v[a].dq[b];
-                }
-                cout << '\n';
-            }
-            int a=  1;
+        for (int i = x; i <= N; i += x) {
+            v[i].shuffle(d, k);
         }
 
         checkAdj();
@@ -144,7 +174,7 @@ int main() {
     int answer = 0;
     for (int i = 1; i <= N; i++) {
         for (int j = 0; j < M; j++) {
-            if (v[i].dq[j] != 0) answer += v[i].dq[j];
+            answer += v[i].dq[j];
         }
     }
     
