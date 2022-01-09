59f717b47d66308a1ff26703a898f8f4b28529e9
rokkyw <rokkyw@naver.com>
2022-01-10 00:00:58 +0900

    완전탐색/연산자끼워넣기_BF_14888.cpp

--- 완전탐색/연산자끼워넣기_BF_14888.cpp
+++ 완전탐색/연산자끼워넣기_BF_14888.cpp
@@ -3,56 +3,51 @@
 #include <vector>
 using namespace std;
 
+int N, num;
+vector<int> arr;
+vector<int> oper;
 
-int calc(vector<int> a, vector<int> operate) {
-    int sum = a[0];
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
 
-    for (int i = 1; i < a.size(); i++) {  //연산자는 피연산자의 한개덜필요함
-        if (operate[i-1] == 0) sum += a[i];
-        else if (operate[i-1] == 1) sum -= a[i];
-        else if (operate[i-1] == 2) sum *= a[i];
-        else sum /= a[i];
+    cin >> N;
+
+    arr.resize(N, 0);
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
     }
-    
-    return sum;
-}
 
-int main(){
-    int num;
-    cin >> num;   //입력할 수의 개수
+    for (int i = 1; i <= 4; i++) {
+        cin >> num;
 
-    vector<int> a;
-    
-    int sample1;
-
-    for (int i = 0; i < num; i++) {
-        cin >> sample1;       //연산하고 싶은 수
-        a.push_back(sample1);
-    }
-    
-    vector<int> operate;
-    for (int i = 0; i < 4; i++) {
-        int cnt;
-        cin >> cnt;   //각 연산자의 개수를 입력
-        
-        for (int k = 0; k < cnt; k++) {   //0, 1, 2, 3의 숫자가 본인의 개수만큼 들어감
-            operate.push_back(i);
+        for (int j = 0; j < num; j++) {
+            oper.push_back(i);
         }
     }
 
-    sort(operate.begin(), operate.end());
-    
-    vector<int> result;
+    int _max = -21e8;
+    int _min = 21e8;
 
     do {
-        int temp = calc(a, operate);
-        result.push_back(temp);
-    } while(next_permutation(operate.begin(), operate.end()));
-    
-    auto ans = minmax_element(result.begin(), result.end());
+        int answer = arr.front();
 
-    cout << *ans.second << '\n';
-    cout << *ans.first << '\n';
-    
+        for (int i = 1; i < arr.size(); i++) {
+            int _oper = oper[i - 1];
+
+            if (_oper == 1) answer += arr[i];
+            else if (_oper == 2) answer -= arr[i];
+            else if (_oper == 3) answer *= arr[i];
+            else answer /= arr[i];
+        }
+
+        _max = max(_max, answer);
+        _min = min(_min, answer);
+        
+    } while (next_permutation(oper.begin(), oper.end()));
+
+    cout << _max << '\n' << _min;
+
     return 0;
 }
