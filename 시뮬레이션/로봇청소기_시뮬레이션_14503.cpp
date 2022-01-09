06ebefcda1b083876e9f6f44296ff89a197074be
rokkyw <rokkyw@naver.com>
2022-01-09 23:09:42 +0900

    시뮬레이션/로봇청소기_시뮬레이션_14503.cpp - 큐로 풀었을 경우

--- 시뮬레이션/로봇청소기_시뮬레이션_14503.cpp
+++ 시뮬레이션/로봇청소기_시뮬레이션_14503.cpp
@@ -1,18 +1,87 @@
 #include <iostream>
-
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <cstring>
 using namespace std;
 
-const int MAX = 101;
-int map[MAX][MAX];
+// 벽을 세 개 세웠을 때의 최대 안전영역의 크기는?
+const int MAX = 51;
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, 1, 0, -1};
 int N, M;
-int r, c;
-int cnt;
-int dir;
+int map[MAX][MAX];
+int tmp_map[MAX][MAX];
+bool clean[MAX][MAX];
+int answer;
+enum {
+    EMPTY, WALL
+};
 
-int main(){
+struct Robot {
+    int x, y, dir;
+};
+
+queue<Robot> q;
+
+void run() {
+
+    while (!q.empty()) {
+        Robot now = q.front();
+        q.pop();
+
+        // 1. 현재 위치 청소
+        if (!clean[now.x][now.y]) answer++;
+        clean[now.x][now.y] = true;
+
+        int dir = now.dir;
+        int nx, ny;
+        int cnt = 0, wall = 0, _clean = 0;
+        bool flag = false;
+
+        // 2. 현재 위치에서 현재 방향을 기준으로 왼쪽 방향부터 차례대로 인접한 칸을 탐색
+        // 0:북, 1:동, 2:남, 3:서
+        while (cnt < 4) {
+            cnt++;
+            dir = dir - 1 < 0 ? 3 : dir - 1;
+
+            nx = now.x + dx[dir];
+            ny = now.y + dy[dir];
+
+            if (nx < 0 || nx < 0 || nx >= N || ny >= M) continue;
+            if (map[nx][ny] == WALL) wall++;
+            if (map[nx][ny] == EMPTY && clean[nx][ny]) _clean++;
+            if (map[nx][ny] == EMPTY && !clean[nx][ny]) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag) {
+            q.push({nx, ny, dir});
+        }
+        else {
+            if (wall <= 4 || _clean <= 4) {
+                int d = now.dir % 2 == 0 ? 2 - now.dir : 4 - now.dir;
+                nx = now.x + dx[d];
+                ny = now.y + dy[d];
+
+                if (map[nx][ny] == EMPTY) q.push({nx, ny, now.dir});
+                else if (map[nx][ny] == WALL) return;
+            }
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
 
     cin >> N >> M;
-    cin >> r >> c >> dir;
+
+    int x, y, dir;
+    cin >> x >> y >> dir;
+    q.push({x, y, dir});
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
@@ -20,52 +89,9 @@ int main(){
         }
     }
 
-    while(1) {
-        int dx[] = {-1, 0, 1, 0};   //북0 동1 남2 서3
-        int dy[] = {0, 1, 0, -1};
+    run();
 
-        if (map[r][c] == 0) {
-            map[r][c] = 2;
-            cnt += 1;
-        }
+    cout << answer;
 
-        int check = 0;
-
-        for (int i = 0; i < 4; i++) { // dir:0-> -1(3) - 2 - 1 - 0
-            dir -= 1;
-
-            if(dir < 0)
-                dir = 3;
-
-            int nx = r + dx[dir];
-            int ny = c + dy[dir];
-
-            if (map[nx][ny] == 0) {
-                r = nx;
-                c = ny;
-                check = 1;
-                break;
-            }
-        }
-
-        if (check == 1)
-            continue;
-        //후진의 경우
-        int tmp_dir = dir - 2;
-        
-        if(tmp_dir == -2)
-            tmp_dir = 2;
-        if(tmp_dir == -1)
-            tmp_dir = 3;
-        
-        r += dx[tmp_dir];
-        c += dy[tmp_dir];
-        
-        if(map[r][c] == 1)
-            break;
-    }
-
-    cout << cnt << '\n';
-    
     return 0;
 }
