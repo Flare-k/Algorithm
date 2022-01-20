fbe689d904146dab799d29206529a241e1658db9
rokkyw <rokkyw@naver.com>
2022-01-20 22:32:26 +0900

    자료구조/후위표기식_자료구조_1918.cpp

--- 자료구조/후위표기식_자료구조_1918.cpp
+++ 자료구조/후위표기식_자료구조_1918.cpp
@@ -7,50 +7,42 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    string oper;
-    cin >> oper;
+    string s;
+    cin >> s;
 
+    stack<char> st;
 
-    int first = 0;
-    int idx = 0;
-
-    while (1) {
-        if (oper[idx] == '*' || oper[idx] == '/') {
-            string firstSec = oper.substr(0, idx - 1);
-            string midSec = oper.substr(idx - 1, 3);
-            string endSec = oper.substr(idx + 2);
-            oper = firstSec + '(' + midSec + ')' + endSec;
-
-            first = idx + 4;
-            idx += 2;
-        }
-
-        idx++;
-        if (idx > oper.length()) break;
-    }
-
-    stack<char> brac;
-    stack<char> operList;
-
-    for (char ch : oper) {
-        if (ch == '(') brac.push('(');
-        else if (ch == ')' && brac.top() == '(') {
-            brac.pop();
-            if (!operList.empty()) {
-                cout << operList.top();
-                operList.pop();
+    for (char c : s) {
+        if (c >= 'A' && c <= 'Z') cout << c;
+        else {
+            if (c == '(') st.push(c);
+            else if (c == ')') {
+                while (!st.empty() && st.top() != '(') {
+                    cout << st.top();
+                    st.pop();
+                }
+                st.pop();   // '(' pop
+            }
+            else if (c == '*' || c == '/') {
+                while (!st.empty() && (st.top() == '*' || st.top() == '/')) {
+                    cout << st.top();
+                    st.pop();
+                }
+                st.push(c);
+            }
+            else if (c == '+' || c == '-') {
+                while (!st.empty() && st.top() != '(') {
+                    cout << st.top();
+                    st.pop();
+                }
+                st.push(c);
             }
         }
-        else if (ch == '+' || ch == '-' || ch == '*' || ch == '/'){
-            operList.push(ch);
-        }
-        else {
-            cout << ch;
-        }
     }
-    while (!operList.empty()) {
-        cout << operList.top();
-        operList.pop();
+
+    while (!st.empty()) {
+        cout << st.top();
+        st.pop();
     }
 
     return 0;
