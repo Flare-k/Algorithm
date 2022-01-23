962b9a31996e935c607a2034560b8561da1ecec8
rokkyw <rokkyw@naver.com>
2022-01-23 18:02:10 +0900

    시뮬레이션/아기상어_구현_16236.cpp - new version

--- 시뮬레이션/아기상어_구현_16236.cpp
+++ 시뮬레이션/아기상어_구현_16236.cpp
@@ -1,103 +1,100 @@
 #include <iostream>
-#include <vector>
 #include <cstring>
+#include <vector>
 #include <queue>
 #include <algorithm>
 using namespace std;
 
-const int dx[] = {-1, 1, 0, 0};
-const int dy[] = {0, 0, -1, 1};
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
 const int MAX = 21;
-int N, M;
-int sec, sharkSize, howManyEat;
-int sea[MAX][MAX];
-int dist[MAX][MAX];
+int N;
+int map[MAX][MAX];
 bool visit[MAX][MAX];
 
-struct Fish {
-    int x, y, size;
+struct Dir {
+    int x, y, dist;
+};
+struct Node {
+    int x, y, size, eat;
 };
 
-Fish shark;
-vector<Fish> eat;
+Node shark;
+vector<Dir> fish;
 
-bool cmp(Fish a, Fish b) {
-    if (a.size < b.size) return true;
-    else if (a.size == b.size) {
+bool cmp(Dir& a, Dir& b) {
+    if (a.dist < b.dist) return true;
+    else if (a.dist == b.dist) {
         if (a.x < b.x) return true;
         else if (a.x == b.x) return a.y < b.y;
     }
     return false;
 }
 
+int run() {
+    memset(visit, false, sizeof(visit));
+    queue<Dir> q;
+    fish.clear();
+
+    q.push({shark.x, shark.y, 0});
+    visit[shark.x][shark.y] = true;
+
+    while (!q.empty()) {
+        Dir now = q.front();
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int nx = now.x + dx[i];
+            int ny = now.y + dy[i];
+
+            if (nx < 1 || nx > N || ny < 1 || ny > N) continue;
+            if (visit[nx][ny]) continue;
+            if (map[nx][ny] > shark.size) continue;
+
+            q.push({nx, ny, now.dist + 1});
+            visit[nx][ny] = true;
+            if (map[nx][ny] != 0 && map[nx][ny] < shark.size) fish.push_back({nx, ny, now.dist + 1});
+        }
+    }
+
+    int dist = 0;
+
+    if (!fish.empty()) {
+        sort(fish.begin(), fish.end(), cmp);
+        Dir f = fish.front();
+        dist = f.dist;
+        map[shark.x][shark.y] = 0;
+        map[f.x][f.y] = 9;
+
+        if (shark.eat + 1 == shark.size) shark = {f.x, f.y, shark.size + 1, 0};
+        else shark = {f.x, f.y, shark.size, shark.eat + 1};
+    }
+
+    return dist;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    
+
     cin >> N;
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cin >> sea[i][j];
-            if (sea[i][j] == 9) {
-                shark = {i, j, 2};
-                sea[i][j] = 0;
-            }
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> map[i][j];
+            if (map[i][j] == 9) shark = {i, j, 2, 0};
         }
     }
+
+    int answer = 0;
 
     while (1) {
-        memset(visit, false, sizeof(visit));
-        memset(dist, 0, sizeof(dist));
-        eat.clear();
-
-        sharkSize = shark.size;
-        visit[shark.x][shark.y] = true;
-        
-        queue<Fish> q;
-        q.push(shark);
-
-        while (!q.empty()) {
-            Fish now = q.front();
-            q.pop();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
-
-                if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-                if (sea[nx][ny] > sharkSize) continue;
-                if (visit[nx][ny] == true) continue;
-
-                visit[nx][ny] = true;
-                dist[nx][ny] = dist[now.x][now.y] + 1;
-                q.push({nx, ny});
-
-                if (sea[nx][ny] != 0 && sea[nx][ny] < sharkSize) {
-                    eat.push_back({nx, ny, dist[nx][ny]});  // 위치, 거리
-                }
-            }
-        }
-
-        Fish tmp;
-        if (eat.size() >= 1) {
-            sort(eat.begin(), eat.end(), cmp);
-
-            tmp = eat.front();
-            shark = {tmp.x, tmp.y, sharkSize};
-            sea[shark.x][shark.y] = 0;
-            howManyEat++;
-            sec += tmp.size;
-        }
+        int dist = run();
+        if (dist != 0) answer += dist;
         else break;
-        
-        if (howManyEat == shark.size) {
-            shark.size += 1;
-            howManyEat = 0;
-        }
     }
 
-    cout << sec;
+    cout << answer;
 
     return 0;
 }
