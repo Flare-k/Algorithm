9f37bdcfe19288fa62199b0f4527eb9d7fceda59
rokkyw <rokkyw@naver.com>
2022-01-26 01:04:52 +0900

    시뮬레이션/연구소3_구현_17142.cpp - Back tracking version.

--- 시뮬레이션/연구소3_구현_17142.cpp
+++ 시뮬레이션/연구소3_구현_17142.cpp
@@ -1,124 +1,124 @@
 #include <iostream>
+#include <cstring>
 #include <algorithm>
 #include <queue>
-#include <vector>
-#include <cstring>
+#define MAX 51;
 using namespace std;
 
-/*
-바이러스 N개가 있는데 그중 M개는 활성상태이다.
-활성 상태일 때 주변을 똑같이 복제시킬 수 있다.
-바이러스를 퍼뜨리는 최소시간 구하기
-M에 대한 모든 케이스 구할것
-*/
 const int dx[] = {-1, 1, 0, 0};
 const int dy[] = {0, 0, -1, 1};
-
-const int MAX = 51;
-int acc[MAX][MAX];
-int inst[MAX][MAX];
+int map[MAX][MAX];
+int tmp[MAX][MAX];
 bool visit[MAX][MAX];
 bool enabled[MAX][MAX];
-
-int N, M;
-struct Dir {
-    int x, y, val;
+bool check[MAX];
+int N, M, virusCnt, cnt, limit;
+int answer = 21e8;
+enum {
+    EMPTY, WALL, VIRUS
+};
+struct Node {
+    int x, y, dist;
 };
 
-vector<Dir> dir;
+vector<Node> virus;
+vector<Node> active;
 
-int run(queue<Dir>& q) {
-    int maxx = 0;
+int run() {
+    memset(visit, false, sizeof(visit));
+    memset(enabled, false, sizeof(enabled));
+    memcpy(tmp, map, sizeof(tmp));
+
+    queue<Node> q;
+    int d = 0;
+    
+    for (Node v : active) {
+        q.push({v.x, v.y, 0});
+        visit[v.x][v.y] = true;
+        enabled[v.x][v.y] = true;
+    }
 
     while (!q.empty()) {
-        Dir now = q.front();
+        Node now = q.front();
         q.pop();
 
         for (int i = 0; i < 4; i++) {
             int nx = now.x + dx[i];
             int ny = now.y + dy[i];
 
-            if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-            if (inst[nx][ny] == 1) continue;
-            if (visit[nx][ny]) continue;
-            
-            if (enabled[now.x][now.y] == true && inst[nx][ny] == 2 && enabled[nx][ny] == false) {
-                enabled[nx][ny] = true;
-            }
+            if (nx < 1 || nx > N || ny < 1 || ny > N) continue;
+            if (visit[nx][ny] || map[nx][ny] == WALL) continue;
 
-            q.push({nx, ny, now.val + 1});
-            acc[nx][ny] = now.val + 1;
+            if (enabled[now.x][now.y] == true && map[nx][ny] == VIRUS && enabled[nx][ny] == false) enabled[nx][ny] = true;
+
+            q.push({nx, ny, now.dist + 1});
+            tmp[nx][ny] = now.dist + 1;
             visit[nx][ny] = true;
-            
-            if (inst[nx][ny] == 0) maxx = max(maxx, acc[nx][ny]);
+
+            if (map[nx][ny] == EMPTY) d = max(d, tmp[nx][ny]);
         }
     }
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (acc[i][j] == 0 && !enabled[i][j]) {
-                return -1;
-            }
+    
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            if (tmp[i][j] == 0 && !enabled[i][j]) return - 1;
         }
     }
-
-    return maxx;
+    
+    if (d == 5) {
+        d += 0;
+    }
+    return d;
 }
+
+void virusCheck() {
+    for (int i = 0; i < virusCnt; i++) {
+        if (check[i]) active.push_back({virus[i].x, virus[i].y, 0});
+    }
+}
+
+void combination(int level, int start) {
+    if (level == M) {
+        virusCheck();
+        
+        int result = run();
+        if (result == -1) cnt++;
+        else answer = min(answer, result);
+        limit++;
+
+        active.clear();
+        return;
+    }
+
+    for (int i = start; i < virusCnt; i++) {
+        if (check[i]) continue;
+        check[i] = true;
+        combination(level + 1, i + 1);
+        check[i] = false;
+    }
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    int viruscnt = 0;
     cin >> N >> M;
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cin >> inst[i][j];
-            if (inst[i][j] == 2) {
-                viruscnt++;
-                dir.push_back({i, j});
-            }
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> map[i][j];
+            if (map[i][j] == VIRUS) virus.push_back({i, j});
         }
     }
 
-    vector<int> rand(viruscnt, 0);
+    virusCnt = virus.size();
 
-    for (int i = viruscnt - M; i < viruscnt; i++) {
-        rand[i] = 1;
-    }
+    combination(0, 0);
 
-    int answer = 21e8;
-    int cnt = 0;
-    int minus = 0;
+    if (limit == cnt) answer = -1;
 
-    do {
-        memset(enabled, false, sizeof(enabled));
-        memset(visit, false, sizeof(visit));
-        memcpy(acc, inst, sizeof(acc));
-        queue<Dir> q;
-
-        for (int i = 0; i < rand.size(); i++) {
-            if (rand[i] == 1) {
-                q.push({dir[i].x, dir[i].y, 0});
-                acc[dir[i].x][dir[i].y] = 3;
-                enabled[dir[i].x][dir[i].y] = true;
-                visit[dir[i].x][dir[i].y] = true;
-            }
-        }
-
-        int result = run(q);
-        
-        if (result == -1) minus++;
-        else answer = min(answer, result);
-
-        cnt++;
-    } while (next_permutation(rand.begin(), rand.end()));
-
-    if (cnt == minus) answer = -1;
-
-    if (answer == 21e8) cout << -1;
-    else cout << answer;
+    cout << answer;
 
     return 0;
 }
