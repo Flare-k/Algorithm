b96d8f68e95fe1e781d73f482dc3a198a6e66fed
Flare-k <rokkyw@naver.com>
2022-03-31 23:34:31 +0900

    시뮬레이션/스타트택시_구현_19238.cpp - renew

--- 시뮬레이션/스타트택시_구현_19238.cpp
+++ 시뮬레이션/스타트택시_구현_19238.cpp
@@ -1,34 +1,27 @@
-// 19238
-// 벽에 가로막힌 경우, 제자리가 택시위치인경우 고려할것
 #include <iostream>
-#include <cstring>
 #include <algorithm>
+#include <cstring>
 #include <vector>
 #include <queue>
+#define MAX 21
 using namespace std;
-
-const int MAX = 21;
-const int dx[5] = {0, -1, 1, 0, 0};
-const int dy[5] = {0, 0, 0, -1, 1};
-int map[MAX][MAX];  // 전체 맵의 구조
-int dist[MAX][MAX]; // 각 승객이 택시를 타고 이동할 때의 거리
+// 북서동남
+const int dx[] = {-1, 1, 0, 0};
+const int dy[] = {0, 0, -1, 1};
+int arr[MAX][MAX];
 bool visit[MAX][MAX];
-int N, M, fuel;
-int tx, ty;     // 택시 위치
-int current;    // 현재 승객의 번호
-struct Node {
-    int x, y, num, dist;    // 좌표xy, 승객번호, 최단거리 값
-    bool exist;             // 승객을 완전히 이동시켰는지
+int N, M, K;
+struct Taxi { int x, y, fuel; };
+struct Dir { int x, y, dist; };
+enum {
+    ROAD, WALL, PASSENGER
 };
 
-struct Dir {
-    int x, y;
-};
+Taxi taxi;
+Dir vec[MAX][MAX];
+bool flag;
 
-vector<Node> start; // 각 승객의 출발 좌표
-vector<Node> dest;  // 각 승객의 도착 좌표
-
-bool cmp(Node& A, Node& B) {
+bool cmp(Dir A, Dir B) {
     if (A.dist < B.dist) return true;
     else if (A.dist == B.dist) {
         if (A.x < B.x) return true;
@@ -37,168 +30,140 @@ bool cmp(Node& A, Node& B) {
     return false;
 }
 
-// 현재 택시 위치에서 각 출발지 거리 탐색
-bool findStartPoint() {
-    memcpy(dist, map, sizeof(dist));
+void findEndPoint(int x, int y, int dist) {
+    // 만약 승객을 태우러갈때 사용된 연료가 바닥이 나면 종료!
+    if (taxi.fuel - dist < 0) {
+        flag = true;
+        return;
+    }
+
+    taxi.fuel -= dist;
+    arr[x][y] = ROAD;
+
+    int ex = vec[x][y].x;
+    int ey = vec[x][y].y;
+
     memset(visit, false, sizeof(visit));
-    
     queue<Dir> q;
-    q.push({tx, ty});
-    visit[tx][ty] = true;   // 택시 좌표와 승객의 좌표가 동일할 수도 있으므로 방문표시
+
+    q.push({x, y, 0});
+    visit[x][y] = true;
+
+    Dir depart; // 도착 지점의 좌표 & 거리
+
+    bool check = false; // 벽에 둘러싸여있는 경우 고려
 
     while (!q.empty()) {
         Dir now = q.front();
         q.pop();
 
-        for (int i = 1; i <= 4; i++) {
+        for (int i = 0; i < 4; i++) {
             int nx = now.x + dx[i];
             int ny = now.y + dy[i];
 
             if (nx < 1 || nx > N || ny < 1 || ny > N) continue;
-            if (visit[nx][ny] || dist[nx][ny] == -1) continue;
+            if (visit[nx][ny] || arr[nx][ny] == WALL) continue;
 
             visit[nx][ny] = true;
-            dist[nx][ny] = dist[now.x][now.y] + 1;
-            q.push({nx, ny});
-        }
-    }
+            q.push({nx, ny, now.dist + 1});
 
-    Node result;
-    int distance = 21e8;
-    vector<Node> tmp;
-
-    for (auto s : start) {
-        if (s.exist == false) continue;
-
-        int x = s.x;
-        int y = s.y;
-
-        if (distance >= dist[x][y] && visit[x][y]) {    // 택시가 출발지점과 도착지점으로 운행을 못할 수도 있음을 고려해 visit 여부 확인
-            tmp.push_back({x, y, s.num, dist[x][y], s.exist});
-            distance = dist[x][y];
-        }
-    }
-
-    bool flag = true;
-
-    if (!tmp.empty()) {
-        sort(tmp.begin(), tmp.end(), cmp);
-        result = tmp.front();
-        tx = result.x;
-        ty = result.y;
-        start[result.num].exist = false;
-        fuel -= dist[tx][ty];
-        current = result.num;
-    }
-    else {
-        flag = false;
-    }
-
-    return flag;
-}
-
-// 각 도착지 거리 탐색
-bool findDestination() {
-    memcpy(dist, map, sizeof(dist));
-    memset(visit, false, sizeof(visit));
-
-    int distance = 0;
-
-    queue<Dir> q;
-    q.push({tx, ty});
-
-    Node destination = dest[current];
-
-    while (!q.empty()) {
-        Dir now = q.front();
-        q.pop();
-
-        for (int i = 1; i <= 4; i++) {
-            int nx = now.x + dx[i];
-            int ny = now.y + dy[i];
-
-            if (nx < 1 || nx > N || ny < 1 || ny > N) continue;
-            if (visit[nx][ny] || dist[nx][ny] == -1) continue;
-
-            visit[nx][ny] = true;
-            dist[nx][ny] = dist[now.x][now.y] + 1;
-            q.push({nx, ny});
-
-            if (nx == destination.x && ny == destination.y) {
-                distance = dist[nx][ny];
-                dest[current].exist = false;
-                tx = nx;
-                ty= ny;
+            if (nx == ex && ny == ey) {
+                depart = {nx, ny, now.dist + 1};
+                check = true;
                 break;
             }
         }
     }
 
-    fuel -= distance;
-    bool flag;
+    // 도착지점까지 도착했다면 taxi의 상황 업데이트
+    if (check && taxi.fuel - depart.dist >= 0) taxi = {depart.x, depart.y, taxi.fuel + depart.dist};
+    else flag = true;
 
-    if (fuel < 0) flag = false;
-    else {
-        fuel += (distance * 2);
-        flag = true;
-    }
-
-    return flag;
 }
 
-bool checkPassenger() {
-    for (int i = 1; i <= M; i++) {
-        if (start[i].exist || dest[i].exist) return false;
+bool findStarttPoint() {
+    // 최단 거리의 승객찾기
+    queue<Dir> q;
+    memset(visit, false, sizeof(visit));
+    
+    q.push({taxi.x, taxi.y, 0});
+    visit[taxi.x][taxi.y] = true;
+
+    vector<Dir> passenger;  // 승객의 출발 위치 벡터
+
+    // 승객이 있는 곳과 택시가 있는 곳이 겹칠 때
+    if (arr[taxi.x][taxi.y] == PASSENGER) passenger.push_back({taxi.x, taxi.y, 0});
+    
+    int cnt = 0;
+
+    while (!q.empty()) {
+        Dir now = q.front();
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int nx = now.x + dx[i];
+            int ny = now.y + dy[i];
+
+            if (nx < 1 || nx > N || ny < 1 || ny > N) continue;
+            if (visit[nx][ny] || arr[nx][ny] == WALL) continue;
+
+            visit[nx][ny] = true;
+            q.push({nx, ny, now.dist + 1});
+
+            if (arr[nx][ny] == PASSENGER) {
+                passenger.push_back({nx, ny, now.dist + 1});
+            }
+        }
     }
-    return true;
+
+    if (passenger.size() > 0) {
+        sort(passenger.begin(), passenger.end(), cmp);
+        Dir node = passenger.front();
+        // 최단 거리의 승객을 태울 경우 도착지점 까지의 연료 변화 구하기
+        findEndPoint(node.x, node.y, node.dist);
+        return true;
+    }
+
+    // 태울 승객이 없다? -> 벽에 가로막혀서 못간다.
+
+    return false;
+}
+
+void init() {
+    cin >> N >> M >> K;
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    
+    int x, y;
+    cin >> x >> y;
+    taxi = {x, y, K};
+
+    for (int i = 0; i < M; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        arr[x1][y1] = PASSENGER;
+        vec[x1][y1] = {x2, y2};
+    }
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
+    cout.tie(0);
 
-    cin >> N >> M >> fuel;
+    init();
 
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
-            cin >> map[i][j];
-            if (map[i][j] == 1) map[i][j] = -1; // wall = -1
-        }
+    for (int i = 0; i < M; i++) {
+        if (!findStarttPoint()) flag = true;
+        if (flag) break;
     }
 
-    // Taxi Point
-    cin >> tx >> ty;
-
-    start.resize(M + 1);
-    dest.resize(M + 1);
-
-    int sx, sy, ex, ey;
-
-    for (int i = 1; i <= M; i++) {
-        cin >> sx >> sy >> ex >> ey;
-        start[i] = {sx, sy, i, 0, true};
-        dest[i] = {ex, ey, i, 0, true};
-    }
-
-    while (1) {
-        bool flagStart = findStartPoint();
-        // 출발점을 못찾았다면
-        if (flagStart == false) {
-            fuel = -1;
-            break;
-        }
-
-        bool flagDest = findDestination();
-        // 연료를 다썼다면
-        if (flagDest == false) {
-            fuel = -1;
-            break;
-        }
-
-        if (checkPassenger()) break;
-    }
-
-    cout << fuel;
+    if (!flag) cout << taxi.fuel << '\n';
+    else cout << -1 << '\n';
 
     return 0;
 }
