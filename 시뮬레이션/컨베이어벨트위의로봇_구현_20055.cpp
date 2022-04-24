bd85f50ee8734a2854ddb1e6af5694683c735de9
Flare-k <rokkyw@naver.com>
2022-04-24 21:13:42 +0900

    시뮬레이션/컨베이어벨트위의로봇_구현_20055.cpp

--- 시뮬레이션/컨베이어벨트위의로봇_구현_20055.cpp
+++ 시뮬레이션/컨베이어벨트위의로봇_구현_20055.cpp
@@ -1,76 +1,71 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <deque>
 using namespace std;
 
-/*
-    내구도가 닳는 경우
-    로봇이 칸에 올라갈 때, 로봇이 칸에서 이동할 때
-*/
-
-struct Block {
+int N, K;
+int checkHp;
+struct Node {
     int hp;
-    bool robot;
+    bool exist;
 };
+
+deque<Node> dq;
+
+void rotateBelt() {
+    dq.push_front(dq.back());
+    dq.pop_back();
+    if (dq[N - 1].exist == true) dq[N - 1].exist = false;  // 로봇 내리기
+}
+
+void liftBox() {
+    if (dq[0].hp > 0 && !dq[0].exist) {
+        dq[0].hp--;
+        dq[0].exist = true;
+        if (dq[0].hp == 0) checkHp++;
+    }
+}
+
+void rotateexist() {
+    for (int i = N - 2; i >= 0; i--) {
+        if (dq[i].exist) {   // 로봇이 올려져 있다면
+            if (dq[i + 1].exist == false && dq[i + 1].hp > 0) {
+                dq[i + 1].exist = true;
+                dq[i + 1].hp--;
+                dq[i].exist = false;
+                if (dq[i + 1].hp == 0) checkHp++;
+            }
+        }
+    }
+    
+    if (dq[N - 1].exist == true) dq[N - 1].exist = false;  // 로봇 내리기
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(0);
+    cout.tie(0);
 
-    int N, K;
     cin >> N >> K;
 
-    Block blocks[N * 2 + 1];
-    Block tmps[N * 2 + 1];
-
-    // 내구도
-    int h;
-    for (int i = 1; i <= N  * 2; i++) {
-        cin >> blocks[i].hp;
-        blocks[i].robot = false;
+    for (int i = 0; i < 2 * N; i++) {
+        int num;
+        cin >> num;
+        dq.push_back({num, false});
     }
 
-    int cnt = 0;
-    int iter = 1;
-    while (iter++) {
-        // 1단계
-        tmps[1] = blocks[N * 2];    // 하나 올리기
-        for (int i = 2; i <= N * 2; i++) {
-            tmps[i] = blocks[i - 1];
-        }
-        for (int i = 1; i <= N * 2; i++) {
-            blocks[i] = tmps[i];
-        }   // blocks로 다시 복사
-        if (blocks[N].robot == true) blocks[N].robot = false; // 로봇 내리기
+    int level = 0;
 
-        // 2단계
-        for (int i = N - 1; i >= 1; i--) {
-            if (blocks[i].robot) {   // 로봇이 올려져 있다면
-                if (blocks[i + 1].robot == false && blocks[i + 1].hp > 0) {
-                    blocks[i + 1].robot = true;
-                    blocks[i + 1].hp--;
-                    blocks[i].robot = false;
-                    if (blocks[i + 1].hp == 0) cnt++;
-                }
-            }
-        }
-        if (blocks[N].robot == true) blocks[N].robot = false;  // 로봇 내리기
-
-        // 3단계
-        if (blocks[1].hp > 0 && !blocks[1].robot) {
-            blocks[1].robot = true;
-            blocks[1].hp--;
-            if (blocks[1].hp == 0) cnt++;
-        }
-
-        // 4단계
-        if (cnt >= K) {
-            break;
-        }
-
+    while (1) {
+        rotateBelt();
+        rotateexist();
+        liftBox();
+        level++;
+        if (checkHp >= K) break;
     }
+    
+    cout << level << '\n';
 
-    cout << iter - 1;
-
-    return 0;
+	return 0;
 }
