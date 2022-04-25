874a7ec24e18891c3962628198942ca86d09cabf
Flare-k <rokkyw@naver.com>
2022-04-25 23:17:06 +0900

    시뮬레이션/모노미노도미노2_구현_20061.cpp - Retry.3

--- 시뮬레이션/모노미노도미노2_구현_20061.cpp
+++ 시뮬레이션/모노미노도미노2_구현_20061.cpp
@@ -1,261 +1,246 @@
 #include <iostream>
-#include <string>
-#include <cstring>
-#include <vector>
+#include <algorithm>
 #define MAX 10
 using namespace std;
 
 int board[MAX][MAX];
-int removeBlueCnt, removeGreenCnt;
-int lightGreen, lightBlue;
-bool lightFlag;
-int score;
-vector<int> green, blue;
-int blocks;
+int N, score;
 
-// 초록색 보드로 이동
-void blockMoveToGreenArea(int t, int x, int y) {
+void moveBlock(int t, int x, int y) {
+    int nx, ny;
+    bool flag;
+
     if (t == 1) {
-        for (int i = x; i < MAX; i++) {
-            if (board[i][y] != 0) {
-                board[i - 1][y] = t;
-                break;
-            }
-            
-            if (board[i][y] == 0 && i == MAX - 1) {
+        flag = false;
+
+        for (int i = x; i < MAX - 1; i++) {
+            if (board[i + 1][y] != 0) {
+                flag = true;
                 board[i][y] = t;
+                break;
             }
         }
-    }
-    else if (t == 2) {
-        for (int i = x; i < MAX; i++) {
-            if (board[i][y] != 0 || board[i][y + 1] != 0) {
-                board[i - 1][y] = t;
-                board[i - 1][y + 1] = t;
+
+        if (!flag) board[MAX - 1][y] = t;
+
+        flag = false;
+        for (int j = y; j < MAX - 1; j++) {
+            if (board[x][j + 1] != 0) {
+                flag = true;
+                board[x][j] = t;
                 break;
             }
+        }
 
-            if (board[i][y] == 0 && board[i][y + 1] == 0 && i == MAX - 1) {
+        if (!flag) board[x][MAX - 1] = t;
+    }
+    else if (t == 2) {  // 1x2
+        flag = false;
+
+        for (int i = x; i < MAX - 1; i++) {
+            if (board[i + 1][y] != 0 || board[i + 1][y + 1] != 0) {
+                flag = true;
                 board[i][y] = t;
                 board[i][y + 1] = t;
-            }
-        }
-    }
-    else if (t == 3) {
-        for (int i = x; i < MAX; i++) {
-            if (board[i][y] != 0) {
-                board[i - 2][y] = t;
-                board[i - 1][y] = t;
                 break;
             }
+        }
 
-            if (board[i][y] == 0 && board[i - 1][y] == 0 && i == MAX - 1) {
-                board[i - 1][y] = t;
+        if (!flag) {
+            board[MAX - 1][y] = t;
+            board[MAX - 1][y + 1] = t;
+        }
+
+        flag = false;
+
+        for (int j = y; j < MAX - 1; j++) {
+            if (board[x][j + 1] != 0) {
+                flag = true;
+                board[x][j] = t;
+                board[x][j - 1] = t;
+                break;
+            }
+        }
+
+        if (!flag) {
+            board[x][MAX - 1] = t;
+            board[x][MAX - 2] = t;
+        }
+    }
+    else if (t == 3) {  // 2x1
+        flag = false;
+
+        for (int i = x; i < MAX - 1; i++) {
+            if (board[i + 1][y] != 0) {
+                flag = true;
                 board[i][y] = t;
-            }
-        }
-    }
-}
-
-// 파란색 보드로 이동
-void blockMoveToBlueArea(int t, int x, int y) {
-    if (t == 1) {
-        for (int j = y; j < MAX; j++) {
-            if (board[x][j] != 0) {
-                board[x][j - 1] = t;
+                board[i - 1][y] = t;
                 break;
             }
-
-            if (board[x][j] == 0 && j == MAX - 1) {
-                board[x][j] = t;
-            }
         }
-    }
-    else if (t == 2) {
-        for (int j = y; j < MAX; j++) {
-            if (board[x][j] != 0) {
-                board[x][j - 1] = t;
-                board[x][j - 2] = t;
-                break;
-            }
 
-            if (board[x][j] == 0 && board[x][j - 1] == 0 && j == MAX - 1) {
-                board[x][j - 1] = t;
-                board[x][j] = t;
-            }
+        if (!flag) {
+            board[MAX - 1][y] = t;
+            board[MAX - 2][y] = t;
         }
-    }
-    else if (t == 3) {
-        for (int j = y; j < MAX; j++) {
-            if (board[x][j] != 0 || board[x + 1][j] != 0) {
-                board[x][j - 1] = t;
-                board[x + 1][j - 1] = t;
-                break;
-            }
 
-            if (board[x][j] == 0 && board[x + 1][j] == 0 && j == MAX - 1) {
+        flag = false;
+
+        for (int j = y; j < MAX - 1; j++) {
+            if (board[x][j + 1] != 0 || board[x + 1][j + 1] != 0) {
+                flag = true;
                 board[x][j] = t;
                 board[x + 1][j] = t;
+                break;
             }
+        }
+
+        if (!flag) {
+            board[x][MAX - 1] = t;
+            board[x + 1][MAX - 1] = t;
         }
     }
 }
 
-// 가득찬 행/열이 있는지 체크하고 해당 행/열 인덱스를 벡터에 저장
-bool checkFullArea() {
-    removeBlueCnt = 0;
-    removeGreenCnt = 0;
-    green.clear();
-    blue.clear();
-
-    // green
-    for (int i = 6; i < MAX; i++) {
-        int cnt = 0;
-        for (int j = 0; j < 4; j++) {
-            if (board[i][j] != 0) cnt++;
-        }
-
-        if (cnt == 4) {
-            removeGreenCnt++;
-            green.push_back(i);
-        }
-    }
-
-    // blue
-    for (int j = 6; j < MAX; j++) {
-        int cnt = 0;
-        for (int i = 0; i < 4; i++) {
-            if (board[i][j] != 0) cnt++;
-        }
-
-        if (cnt == 4) {
-            removeBlueCnt++;
-            blue.push_back(j);
-        }
-    }
-
-    if (removeBlueCnt != 0 || removeGreenCnt != 0) return true;
-    return false;
-}
-
-void moveBlock() {
-    score += green.size();
-    score += blue.size();
-
-    if (removeGreenCnt != 0) {
-        for (int col : green) {
-            for (int i = col; i >= 4; i--) {
-                for (int j = 0; j < 4; j++) {
-                    board[i][j] = board[i - 1][j];
-                }
-            }
-        }
-    }
-
-    if (removeBlueCnt != 0) {
-        for (int row : blue) {
-            for (int j = row; j >= 4; j--) {
-                for (int i = 0; i < 4; i++) {
-                    board[i][j] = board[i][j - 1];
-                }
-            }
-        }
-    }
-}
-
-// 연한 영역에 있는지 체크
-void lightArea() {
-    lightFlag = false;
-    lightGreen = 0;
-    lightBlue = 0;
-
+void checkGreenMuddyArea(int& green) {
+    // 초록의 연한 곳
     for (int i = 4; i <= 5; i++) {
-        int cnt = 0;
         for (int j = 0; j < 4; j++) {
             if (board[i][j] != 0) {
-                lightFlag = true;
-                cnt++;
+                green++;
+                break;
             }
         }
-
-        if (cnt > 0) lightGreen++;
     }
+}
 
+void checkBlueMuddyArea(int& blue) {
+    // 파랑의 연한 곳
     for (int j = 4; j <= 5; j++) {
-        int cnt = 0;
         for (int i = 0; i < 4; i++) {
             if (board[i][j] != 0) {
-                lightFlag = true;
-                cnt++;
-            }
-        }
-
-        if (cnt > 0) lightBlue++;
-    }
-}
-
-// 연한 영역에 있는 경우 한 칸씩 밀기
-void lightMove() {
-    while (lightGreen--) {
-        for (int i = MAX - 1; i >= 4; i--) {
-            for (int j = 0; j < 4; j++) {
-                board[i][j] = board[i - 1][j];
-            }
-        }
-    }
-
-    while (lightBlue--) {
-        for (int j = MAX - 1; j >= 4; j--) {
-            for (int i = 0; i < 4; i++) {
-                board[i][j] = board[i][j - 1];
+                blue++;
+                break;
             }
         }
     }
 }
 
-void checkBlockCnt() {
-    for (int i = 6; i < MAX; i++) {
+bool checkGreenArea(int& cnt, int& row) {
+    // 초록의 가득찬 행
+    bool flag = false;
+
+    for (int i = MAX - 1; i >= 6; i--) {
+        int line = 0;
         for (int j = 0; j < 4; j++) {
-            if (board[i][j] != 0) blocks++;
+            if (board[i][j] != 0) line++;
+        }
+
+        if (line == 4) {
+            row = max(row, i);
+            cnt++;
+            flag = true;
         }
     }
 
-    for (int j = 6; j < MAX; j++) {
+    if (flag) score += cnt;
+
+    return flag;
+}
+
+bool checkBlueArea(int& cnt, int& col) {
+    // 파랑의 가득찬 열
+    bool flag = false;
+
+    for (int j = MAX - 1; j >= 6; j--) {
+        int line = 0;
         for (int i = 0; i < 4; i++) {
-            if (board[i][j] != 0) blocks++;
+            if (board[i][j] != 0) line++;
+        }
+
+        if (line == 4) {
+            col = max(col, j);
+            cnt++;
+            flag = true;
         }
     }
+
+    if (flag) score += cnt;
+
+    return flag;
+}
+
+void pullGreenArea(int& cnt, int& row) {
+    for (int k = 0; k < cnt; k++) {
+        for (int i = row - 1; i >= 3; i--) {
+            for (int j = 0; j < 4; j++) {
+                board[i + 1][j] = board[i][j];
+            }
+        }
+    }
+}
+
+void pullBlueArea(int& cnt, int& col) {
+    for (int k = 0; k < cnt; k++) {
+        for (int j = col - 1; j >= 3; j--) {
+            for (int i = 0; i < 4; i++) {
+                board[i][j + 1] = board[i][j];
+            }
+        }
+    }
+}
+
+void findAnswer() {
+    int answer= 0;
+
+    for (int i = 0; i < MAX; i++) {
+        for (int j = 0; j < MAX; j++) {
+            if (board[i][j] != 0) answer++;
+        }
+    }
+
+    cout << answer << '\n';
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
+    cout.tie(0);
 
-    int N;
     cin >> N;
-    
-    while (N--) {
+
+    for (int i = 0; i < N; i++) {
         int t, x, y;
         cin >> t >> x >> y;
 
-        // 1. 블록 이동
-        blockMoveToGreenArea(t, x, y);
-        blockMoveToBlueArea(t, x, y);
+        moveBlock(t, x, y);
 
-        // 2. 가득찬 경우 체크
-        while (checkFullArea()) {
-            moveBlock();
+        int cnt, row, col;
+
+        cnt = 0;
+        row = 0;
+        while (checkGreenArea(cnt, row)) {
+            pullGreenArea(cnt, row);
         }
 
-        // 3. 연한 영역 체크
-        lightArea();
-        if (lightFlag) lightMove();
+        cnt = 0;
+        col = 0;
+        while (checkBlueArea(cnt, col)) {
+            pullBlueArea(cnt, col);
+        }
+
+        int green = 0, blue = 0;    // 땡겨야할 줄의 수
+        checkGreenMuddyArea(green);
+        checkBlueMuddyArea(blue);
+
+        row = 9;
+        col = 9;
+        pullGreenArea(green, row);
+        pullBlueArea(blue, col);
     }
 
-    checkBlockCnt();
-
     cout << score << '\n';
-    cout << blocks << '\n';
+    findAnswer();
 
-    return 0;
+	return 0;
 }
